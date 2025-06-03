fedb94d1d64859bf46095d22549cdf305c17a8a6
Diana Duta <diana97duta@gmail.com>
2025-06-03 03:28:56 +0200

    practica 3 ok

--- react.2/src/App.tsx
+++ react.2/src/App.tsx
@@ -1,11 +1,34 @@
-import MiComponente from "./components/MiComponente";
+import { CssBaseline, Container, Grid } from "@mui/material";
+import AppHeader from "./components/AppHeader";
+import BalanceCard from "./components/BalanceCard";
+import TransactionsList from "./components/TransactionList";
+
 import './App.css'
+
+
 
 function App() {
 
   return (
+
     <>
-       <MiComponente />
+      <CssBaseline />
+      <AppHeader />
+
+      <Container sx={{ py: 4 }}>
+        <Grid container
+            spacing={4}
+            justifyContent="center"
+            alignItems="stretch"
+        >
+          <Grid item xs={12} sm={6} md={4}>
+            <BalanceCard />
+          </Grid>
+          <Grid item xs={12} sm={6} md={8}>
+            <TransactionsList />
+          </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }
