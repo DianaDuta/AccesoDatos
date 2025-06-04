296fa138b7fd1cbe4cc76d77eb5041b5d8261430
Diana Duta <diana97duta@gmail.com>
2025-06-04 19:51:39 +0200

    mejorable

--- react.2/src/App.tsx
+++ react.2/src/App.tsx
@@ -1,23 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import DogsPage from './pages/DogsPage';
-
-import './App.css'
-
+import Tablero from './components/Tablero';
+import { Box, CssBaseline } from '@mui/material';
+//import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* página principal  ->  DogsPage */}
-        <Route path="/" element={<DogsPage />} />
-
-        {/* también /dogs */}
-        <Route path="/dogs" element={<DogsPage />} />
-
-        {/* cualquier otra ruta redirige a / */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <CssBaseline />
+      <Box sx={{ mt: 4}}>
+        <Tablero />
+      </Box>
+    </>
   );
 }
 
